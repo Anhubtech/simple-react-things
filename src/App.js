04e247018a0671d340923f0e8f038bb4d02426c3
Anechka <76115701+Anhubtech@users.noreply.github.com>
2023-01-30 03:01:15 +0400

    feat: add files

---
+++ src/App.js
@@ -0,0 +1,66 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<table>
+<header className="header">
+         <p>
+         Leukocytes
+         </p>    
+      </header>
+  <tr>
+    <th>Cell Type</th>
+    <th>Description</th>
+    <th>Cells</th>
+    <th>Span</th>
+    <th>Function</th>
+  </tr>
+
+<tr>
+  <td>Monocyte</td>
+  <td>U shaped, grey or blue cytoplasm</td>
+  <td>100-700</td>
+  <td>per months</td>
+  <td>phagocytosis</td>
+</tr>
+
+<tr>
+  <td>Lymphocyte</td>
+  <td>nucleus spherical, pale blue cytoplasm</td>
+  <td>1500-3000</td>
+  <td>hours to years</td>
+  <td>mount immune response by direct cell attack</td>
+</tr>
+
+<tr>
+  <td>Basophil</td>
+  <td>nucleus lobed, large blue-purple cytoplasm granules</td>
+  <td>20-50</td>
+  <td>hours to days</td>
+  <td>release histamine and other mediators of inflammation, countain heparine</td>
+</tr>
+
+<tr>
+  <td>Basophil</td>
+  <td>nucleus bilobed, red cytoplasm granules</td>
+  <td>100-400</td>
+  <td>about 5 days</td>
+  <td>kill parasitic worms, huge role in allergy and astma process</td>
+</tr>
+
+<tr>
+  <td>Neutrophil</td>
+  <td>nucleus multilobed, inconspicuous cytoplasm granules</td>
+  <td>3000-7000</td>
+  <td>hours to days</td>
+  <td>phagocytize bacteria</td>
+</tr>
+
+</table>
+
+    </div>
+  ); 
+}
+  export default App;
